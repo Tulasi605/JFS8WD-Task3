3af9f33d24940f64fd0fa5354052c2831ad96538
Tulasi <70065424+Tulasi605@users.noreply.github.com>
2024-05-10 09:58:11 +0530

    Update Employee.java

--- Question2/Employee.java
+++ Question2/Employee.java
@@ -5,14 +5,11 @@ public class Employee implements Taxable {
 	    private String name;
 	    private double salary;
 
-	    // Constructor
 	    public Employee(int empId, String name, double salary) {
 	        this.empId = empId;
 	        this.name = name;
 	        this.salary = salary;
 	    }
-
-	    // Implementing calcTax method for income tax calculation
 	    public double calcTax() {
 	        return salary * incomeTax;
 	    }
