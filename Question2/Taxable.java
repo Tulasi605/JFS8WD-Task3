69bc44a01c23ae25790f05315a714b6b6969ea80
Tulasi <70065424+Tulasi605@users.noreply.github.com>
2024-05-10 09:57:21 +0530

    Update Taxable.java

--- Question2/Taxable.java
+++ Question2/Taxable.java
@@ -1,7 +1,7 @@
 package TASK3.Question2;
 
 public interface Taxable {
-	double salesTax = 0.75; // 75%
-    double incomeTax = 0.105; // 10.5%
+	double salesTax = 0.75;
+    double incomeTax = 0.105;
     double calcTax(); 
 }
